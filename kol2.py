3a53e54cc0dc89fa29bb07c7d9a2f58db613ae33
Hector Lozano <vlozano@stud205.ftj.agh.edu.pl>
2017-03-29 18:31:17 +0200

    School program

--- kol2.py
+++ kol2.py
@@ -14,3 +14,46 @@
 # If you have enough courage and time, try storing (reading/writing) 
 # data in text files (YAML, JSON).
 # If you have even more courage, try implementing user interface.
+
+import sys
+
+names = []
+	
+def get_average(marks):
+	total_add = 0;
+	for mark in marks:
+		total_add = total_add + mark
+		
+	print(total_add)	
+	return float(total_add) / float(len(marks))
+		
+def name_holder(name, surname):
+	if type(name) == str and type(surname) == str:
+		names.append(str(name + ' ' + surname))
+	else:
+		print('Impossible to store the name. Name and Surname should be strings')
+		
+			
+def check_attendance(attendance):
+	total_attendance = 0
+	for value in attendance:
+		if value is (1 or 0):
+			pass
+		else:
+			sys.exit('Not valid value in Attendance list')
+			
+		total_attendance += day
+		return total_attendance			
+	
+	
+if __name__ == '__main__':
+	marks = [1, 5, 4, 3 ,5]
+	name = 'Hector'
+	surname = 'Blanco'
+	attendance = [0,1,1,1,0,0,1,0,1,1,1,1,1,0]
+	
+	
+
+
+
+
